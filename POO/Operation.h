5e080d209f7567bb18d49767a13c149b3dc4e4e3
mehdi0001 <86607817+mehdi0001@users.noreply.github.com>
2023-04-08 14:12:22 -0400

    Operation ChefBloc

---
+++ POO/Operation.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Salle.h"
+#include "Patient.h"
+#include <string>
+#include <vector>
+#include <chrono>
+class Operation
+{
+public:
+	Operation(std::vector<std::string> Liste_Employe, Salle* Salle, std::chrono::system_clock::time_point date, float Duree, float HeureDebut, Patient* Patient, std::string Commentaire);
+
+private:
+	std::vector<std::string> Liste_Employe_;
+	Salle* Salle_;
+	std::chrono::system_clock::time_point date_;
+	float Duree_;
+	float HeureDebut_;
+	Patient* Patient_;
+	std::string Commentaire_;
+};
+
+class Operation
+{
+};
+
