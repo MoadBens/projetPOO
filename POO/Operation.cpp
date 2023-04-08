8ce0d7ff62d33ee3ac512fcb0b7b15928de81a07
MoadBens <benslimanemoad@gmail.com>
2023-04-08 15:09:52 -0400

    Updates

---
+++ POO/Operation.cpp
@@ -0,0 +1,14 @@
+#include "Operation.h"
+
+using namespace std;
+
+Operation::Operation(std::vector<std::string> Liste_Employe, Salle* Salle, std::chrono::system_clock::time_point date, float Duree, float HeureDebut, Patient* Patient, std::string Commentaire)
+{
+	Liste_Employe_ = Liste_Employe;
+	Salle_ = Salle;
+	date_ = date;
+	Duree_ = Duree;
+	HeureDebut_ = HeureDebut;
+	Patient_ = Patient;
+	Commentaire_ = Commentaire;
+}
