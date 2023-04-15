8757dcc84e17db40cd92ce5426363d29c92d356d
MoadBens <benslimanemoad@gmail.com>
2023-04-14 21:33:52 -0400

    SQL QUERIES UPDATED

--- POO/POO.cpp
+++ POO/POO.cpp
@@ -14,7 +14,13 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+	Admin* ad = new Admin(1,"MOMO","MOMO","test");
+	
 	SystemeAuthentification* a = new SystemeAuthentification("hello");
+	Admin* admin= new Admin(2,"Mohamed","ZAML","KHIKHI");
+	admin->AjouterUtilisateur("SEGUIN", "SARA", 2, "97BA");
+	admin->AjouterUtilisateur("Mohamed", "ZAML", 2, "97BA");
+	admin->SupprimerUtilisateur("Mohamed","ZAML");
 	vector<Personnel*> users;
 	users = a->FetchUsers();
 	std::cout << users[0]->get_nom() << std::endl;
